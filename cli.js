018ee6ea633483b2a6238935285a414bdffbd630
Anara-carolina <anaramartins21@gmail.com>
2023-09-13 23:22:51 -0300

    projeto finalizado

--- cli.js
+++ cli.js
@@ -3,24 +3,19 @@ const path = require('path');
 const mdLinks = require('./index.js');
 const chalk = require('chalk');
 
-// Diretório que contém os arquivos Markdown
-const directoryPath = './'; // Ajuste o diretório conforme necessário
+const directoryPath = './';
 
-// Lê o diretório e obtém uma lista de arquivos Markdown
 fs.readdir(directoryPath, (err, files) => {
   if (err) {
     console.error('Erro ao ler o diretório:', err);
     return;
   }
 
-  // Filtra apenas os arquivos com extensão .md
   const markdownFiles = files.filter((file) => path.extname(file) === '.md');
 
-  // Itera sobre cada arquivo Markdown encontrado
   markdownFiles.forEach((file) => {
     const filePath = path.join(directoryPath, file);
 
-    // Chama a função mdLinks com o caminho do arquivo
     mdLinks(filePath)
       .then((links) => {
         console.log(`Links no arquivo ${file}:`);
@@ -34,17 +29,14 @@ fs.readdir(directoryPath, (err, files) => {
   });
 });
 
-const filePath = process.argv[2]; // Obtém o caminho a partir dos argumentos da linha de comando
+const filePath = process.argv[2]; 
 
-// Verifica se a opção --validate foi fornecida
 const validateOptionIndex = process.argv.indexOf('--validate');
 const validate = validateOptionIndex !== -1;
 
-// Verifica se a opção --stats foi fornecida
 const statsOptionIndex = process.argv.indexOf('--stats');
 const stats = statsOptionIndex !== -1;
 
-// Chama a função mdLinks com o caminho do arquivo e as opções
 mdLinks(filePath, { validate, stats })
   .then((links) => {
     let totalValidated = 0;
@@ -78,5 +70,6 @@ mdLinks(filePath, { validate, stats })
     console.error(chalk.red('Erro:', error.message));
   });
 
-// Exportando o mdLinks para ser utilizado em outros arquivos (opcional)
+
 module.exports = mdLinks;
+
